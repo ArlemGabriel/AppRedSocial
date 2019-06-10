e3dc1bd5c98caec31c59d82a188a0e47090663d2
ArlemGabriel <jogabra16@gmail.com>
2019-06-10 16:23:51 -0600

    Corrección de referencia

--- app/src/main/java/com/example/appredsocial/Referencias/ReferenciasFirebase.java
+++ app/src/main/java/com/example/appredsocial/Referencias/ReferenciasFirebase.java
@@ -2,5 +2,5 @@ package com.example.appredsocial.Referencias;
 
 public class ReferenciasFirebase {
     final static public String REFERENCIA_USUARIO = "Usuarios";
-    final static public String REFERENCIA_POSTS= "POST";
+    final static public String REFERENCIA_POSTS= "Posts";
 }
