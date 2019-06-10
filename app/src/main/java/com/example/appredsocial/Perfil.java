587a1378d2c622257a88382f95ada3ed5288b15a
Alejandro Garita <luisgc2009@gmail.com>
2019-06-09 23:55:25 -0600

    Implementación y código del carrusel
    El carrusel solo va de una en una foto, se puede cambiar (se como hacerlo) pero se complica más de lo que debería

--- app/src/main/java/com/example/appredsocial/Perfil.java
+++ app/src/main/java/com/example/appredsocial/Perfil.java
@@ -1,16 +1,22 @@
 package com.example.appredsocial;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 public class Perfil extends AppCompatActivity {
     private TextView txtNombre;
+    private CarouselView carouselView;
     FirebaseAuth firebaseAuth;
 
 
@@ -24,8 +30,30 @@ public class Perfil extends AppCompatActivity {
         txtNombre = findViewById(R.id.txtNombre);
 
         String current = firebaseAuth.getCurrentUser().getEmail();
-
         txtNombre.setText(current);
+
+        /*Codigo del carrusel de fotos
+        carouselView = findViewById(R.id.carrouselView);
+        final Drawable[] sampleImages = {};             //Las fotos que aparecen en el carrusel de fotos
+
+        carouselView.setPageCount(sampleImages.length); //La cantidad de paginas del carrusel
+
+        // Que hace cuando se cambia la imagen
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageDrawable(sampleImages[position]);
+            }
+        });
+
+        // Que hace cuando toca una de las imagenes
+        carouselView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                //Abrir pantalla de esa foto
+            }
+        });*/
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
