b2bb045e25e82806aa1661c468bd86eb1f28881c
ArlemGabriel <jogabra16@gmail.com>
2019-06-10 22:35:05 -0600

    Funcionalidad para agregar imagen al editar perfil

--- app/src/main/java/com/example/appredsocial/EditarPerfil.java
+++ app/src/main/java/com/example/appredsocial/EditarPerfil.java
@@ -1,18 +1,26 @@
 package com.example.appredsocial;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class EditarPerfil extends AppCompatActivity {
 
     RadioGroup radioGroup;
     RadioButton radioButton;
     Button btnAceptar;
+    ImageView imgFotoPerfil;
+    private Uri uriImagen;
+    private static final int PICK_IMAGE_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,7 @@ public class EditarPerfil extends AppCompatActivity {
 
         btnAceptar = findViewById(R.id.btnAceptar);
         radioGroup = findViewById(R.id.radio_group);
+        imgFotoPerfil = findViewById(R.id.imgFotoperfil);
 
         btnAceptar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,9 +38,35 @@ public class EditarPerfil extends AppCompatActivity {
             }
         });
 
+        imgFotoPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirBusquedaImg();
+            }
+        });
+
     }
     public void checkButton(View v){
         int radioId = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(radioId);
     }
+    private void abrirBusquedaImg(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data !=null && data.getData() != null){
+            uriImagen = data.getData();
+            Picasso.with(this).load(uriImagen).into(imgFotoPerfil);
+
+        }
+    }
+
 }
