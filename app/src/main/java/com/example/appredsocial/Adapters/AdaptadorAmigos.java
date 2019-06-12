bdf649ac3e46f90ea5bf3a06ac08b090d42f4676
Alejandro Garita <luisgc2009@gmail.com>
2019-06-12 01:45:23 -0600

    Amigos a medias

--- app/src/main/java/com/example/appredsocial/Adapters/AdaptadorAmigos.java
+++ app/src/main/java/com/example/appredsocial/Adapters/AdaptadorAmigos.java
@@ -3,64 +3,80 @@ package com.example.appredsocial.Adapters;
 import android.content.Context;
 import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.appredsocial.Objetos.Amigo;
 import com.example.appredsocial.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
-import java.util.List;
+import java.util.ArrayList;
 
-public class AdaptadorAmigos extends
-        RecyclerView.Adapter<AdaptadorAmigos.ContenedorAmigos> {
+public class AdaptadorAmigos extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    Context context;
+    ArrayList<Amigo> amigos;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
-    private Context contexto;
-    private List<String> mNombres;
-
-    public AdaptadorAmigos(Context context, List<String> nombres){
-        contexto = context;
-        mNombres = nombres;
+    public AdaptadorAmigos(Context context, ArrayList<Amigo> amigos, FirebaseAuth firebaseAuth,
+                           FirebaseFirestore firebaseFirestore){
+        Log.i("Info", "Inicializa el adaptador");
+        for (int i = 0; i<amigos.size(); i++){
+            Log.i("Info", "\t"+amigos.get(i).getNombreCompleto());
+        }
+        this.context = context;
+        this.amigos = amigos;
+        this.firebaseAuth = firebaseAuth;
+        this.firebaseFirestore = firebaseFirestore;
     }
 
     @Override
-    public ContenedorAmigos onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.i("Info", "Inicializa");
         LayoutInflater inflater = LayoutInflater.from(context);
-
-        View vista = inflater.inflate(R.layout.item_amigos, parent, false);
-
-        ContenedorAmigos viewHolder = new ContenedorAmigos(vista);
-        return viewHolder;
+        View row = inflater.inflate(R.layout.item_amigos, parent, false);
+        Item item = new Item(row);
+        return item;
     }
 
     @Override
-    public void onBindViewHolder(ContenedorAmigos holder, int position) {
-        String nombre = mNombres.get(position);
-        holder.textNombre.setText(nombre);
-        //holder.imgFoto.setImageResource();
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        ((Item)holder).textNombre.setText(amigos.get(position).getNombreCompleto());
+        String url = amigos.get(position).getUrlImagen();
+        if(!url.isEmpty() && !url.equals("null")){
+            ((Item)holder).imgFoto.getLayoutParams().height=450;
+            ((Item)holder).imgFoto.requestLayout();
+            Picasso.with(context).load(url).centerInside().fit().into(((Item)holder).imgFoto);
+        }else{
+            ((Item)holder).imgFoto.setImageURI(null);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(amigos == null){ return 0; }
+        else{ return amigos.size(); }
     }
 
     public void deleteAmigo(int posicion){
         //Borrar amigo de la base de datos
     }
 
-    public class ContenedorAmigos extends RecyclerView.ViewHolder{
-        public TextView textNombre;
-        public ImageView imgFoto;
+    public class Item extends RecyclerView.ViewHolder{
+        TextView textNombre;
+        ImageView imgFoto;
 
-        public ContenedorAmigos(View viewItem){
+        public Item(View viewItem){
             super(viewItem);
 
-            textNombre = viewItem.findViewById(R.id.txtNombreAmigos);
-            imgFoto = viewItem.findViewById(R.id.imgFotoAmigos);
-
+            textNombre = (TextView) viewItem.findViewById(R.id.txtNombreAmigos);
+            imgFoto = (ImageView) viewItem.findViewById(R.id.imgFotoAmigos);
         }
     }
 
