a708deba067ade702bb58b56bd3acc1a3247f232
ArlemGabriel <jogabra16@gmail.com>
2019-06-12 05:51:02 -0600

    Mostrar solicitudes de amistad

---
+++ app/src/main/java/com/example/appredsocial/Adapters/AdaptadorSolicitudes.java
@@ -0,0 +1,95 @@
+package com.example.appredsocial.Adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.appredsocial.Objetos.Solicitud;
+import com.example.appredsocial.R;
+import com.example.appredsocial.Referencias.ReferenciasFirebase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class AdaptadorSolicitudes extends RecyclerView.Adapter<AdaptadorSolicitudes.ContenedorSolicitudes>{
+
+    private Context contexto;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private ArrayList<Solicitud> solicitudes;
+
+    public AdaptadorSolicitudes(Context contexto, ArrayList<Solicitud> solicitudes, FirebaseFirestore firebaseFirestore, FirebaseAuth firebaseAuth){
+        this.contexto = contexto;
+        this.solicitudes = solicitudes;
+        this.firebaseFirestore = FirebaseFirestore.getInstance();
+        this.firebaseAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public ContenedorSolicitudes onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(contexto).inflate(R.layout.item_solicitudes,parent,false);
+        return new ContenedorSolicitudes(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final ContenedorSolicitudes holder, int position) {
+
+        firebaseFirestore.collection(ReferenciasFirebase.REFERENCIA_PERFILES).document(solicitudes.get(position).getCorreo()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                        String nombreCompleto = documentSnapshot.getString("Nombre")+" "+documentSnapshot.getString("Apellidos");
+                        holder.textViewNombre.setText(nombreCompleto);
+                        String urlFirebase = documentSnapshot.getString("Url");
+                        if(!urlFirebase.isEmpty()){
+                            Picasso.with(contexto).load(urlFirebase).fit().centerCrop().into(((ContenedorSolicitudes) holder).imageView);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("Error: ",e.getMessage());
+                        //Toast.makeText(PerfilFragment.this,"Carga de perfil fallida",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return solicitudes.size();
+    }
+
+    public Context getContexto() {
+        return contexto;
+    }
+    /*public void deleteItem(int position){
+        String key = keys.get(position);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(ReferenciasFirebase.REFERENCIA_BASEDATOSFIREBASE);
+        ref.child(key).removeValue();
+    }*/
+    public class ContenedorSolicitudes extends RecyclerView.ViewHolder{
+        public TextView textViewNombre;
+        public ImageView imageView;
+        public ContenedorSolicitudes(View itemView){
+            super(itemView);
+            textViewNombre = itemView.findViewById(R.id.nombreSolicitudAmigo);
+            imageView = itemView.findViewById(R.id.imageViewSolicitud);
+        }
+    }
+}
+
--- app/src/main/java/com/example/appredsocial/NotificacionesFragment.java
+++ app/src/main/java/com/example/appredsocial/NotificacionesFragment.java
@@ -1,16 +1,74 @@
 package com.example.appredsocial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.appredsocial.Adapters.AdaptadorSolicitudes;
+import com.example.appredsocial.Objetos.Solicitud;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
 public class NotificacionesFragment extends Fragment {
+    View rootView;
+    private RecyclerView recyclerView;
+    private ArrayList<Solicitud> solicitudes = new ArrayList<>();
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+    private AdaptadorSolicitudes adaptador;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notificaciones,container,false);
+        rootView= inflater.inflate(R.layout.fragment_notificaciones, container, false);
+        recyclerView = rootView.findViewById(R.id.recyclerViewSolicitudes);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        cargarSolicitudes();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                adaptador.notifyDataSetChanged();
+            }
+        }, 500);
+        return rootView;
     }
+    public void cargarSolicitudes(){
+        firebaseFirestore.collection("Solicitudes")
+                .document(firebaseAuth.getCurrentUser().getEmail())
+                .collection("Pendientes")
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                    String correo = documentSnapshot.getString("Correo");
+                    Solicitud solicitud = new Solicitud();
+                    solicitud.setCorreo(correo);
+                    solicitudes.add(solicitud);
+                }
+            }
+        });
+        adaptador = new AdaptadorSolicitudes(getContext(),solicitudes,firebaseFirestore,firebaseAuth);
+        recyclerView.setAdapter(adaptador);
+
+    }
+
+
 }
